a0ea4c018ae571cd6995e10801bed5b957bc1e4e
Nagbhushan shas3 <shas3@localhost.localdomain>
2021-06-17 12:38:34 +0530

    done

---
+++ stuff/divNum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class divNum{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int initNum = in.nextInt();
+        int rem = initNum%10;
+        System.out.print();
+    }
+}
